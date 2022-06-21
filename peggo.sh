1443a0f4635876f9ad65ffc869e069622639a65c
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2022-06-21 17:33:27 +0300

    Create peggo.sh

---
+++ peggo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Peggo Notificator
+
+while true
+do
+        date
+
+heighestEventNonce=$(go run $HOME/peggo_health_check.go | jq ."heighestEventNonce")
+
+echo  "Heighest EventNonce $heighestEventNonce"
+
+ownEventNonce=$(go run $HOME/peggo_health_check.go | jq ."ownEventNonce")
+
+echo "Your EventNonce is $ownEventNonce"
+
+                if [ $heighestEventNonce -ne $ownEventNonce ]; then
+        sendmail -F Peggo -t latflat@gmail.com  < $HOME/email.txt 2> "/dev/null"
+
+                echo  ">>> Notification Email was sent! <<<"
+
+                else
+                        echo  "*** ALL GOOD ***"
+
+                fi
+        sleep 3h
+
+done
