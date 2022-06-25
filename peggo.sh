15540f5370f85db9a316d0fa600ac2c71a9ffdc8
Alexey <87909854+AlexToTheMoon@users.noreply.github.com>
2022-06-25 15:32:46 +0300

    Update peggo.sh

--- peggo.sh
+++ peggo.sh
@@ -5,23 +5,29 @@ while true
 do
         date
 
-heighestEventNonce=$(go run $HOME/peggo_health_check.go | jq ."heighestEventNonce")
+heighestEventNonce=$(go run $HOME/peggo-checker/peggo_health_check.go | jq ."heighestEventNonce")
 
 echo  "Heighest EventNonce $heighestEventNonce"
 
-ownEventNonce=$(go run $HOME/peggo_health_check.go | jq ."ownEventNonce")
+ownEventNonce=$(go run $HOME/peggo-checker/peggo_health_check.go | jq ."ownEventNonce")
 
 echo "Your EventNonce is $ownEventNonce"
+                                        sleep 2s
 
-                if [ $heighestEventNonce -ne $ownEventNonce ]; then
-        sendmail -F Peggo -t USERNAME@gmail.com  < $HOME/email.txt 2> "/dev/null"
+                if [ -z "$heighestEventNonce" ]; then
+        sendmail -F Peggo -t latflat@gmail.com  < $HOME/peggo-checker/get-error.txt 2> "/dev/null"
+                        echo  ">>> Can`t get Event Nonce! Alert was sent. <<<"
+                        
+                elif [ $heighestEventNonce -ne $ownEventNonce ]; then
+        sendmail -F Peggo -t latflat@gmail.com  < $HOME/peggo-checker/sync-error.txt 2> "/dev/null"
 
-                echo  ">>> Notification Email was sent! <<<"
+                echo  ">>> Peggo not synced! Alert was sent! <<<"
 
                 else
                         echo  "*** ALL GOOD ***"
-
+                        echo Next check in 3 hours.
                 fi
         sleep 3h
 
 done
+
